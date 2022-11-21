7eb202c84d575cf844528401c5852ce40bfdb89a
leeminkyu1212 <leeminkyu1212@gmail.com>
2022-11-21 23:50:17 +0900

    test

--- utils/split_csv.py
+++ utils/split_csv.py
@@ -1,13 +1,14 @@
 import pandas as pd
 from os import path as osp
 
-def split_csv(args, path):
+def split_csv(args, path,id):
     data_names = ['ACC','GYRO', 'HeartBeat','GPS','GAME RV']
     index_positions = []
-    df = pd.read_csv(osp.join(args.root_dir, path), header= None)
+    df = pd.read_csv(osp.join(args.root_dir, path+str(id)+'.csv'), header= None)
 
     for data_name in data_names:
         # if data_name != data_names[0]:
+        #if len(index_positions)!=0:
         index_positions.append(df[df.iloc[:,0] == data_name].index[0])
 
     tables = {}
@@ -23,7 +24,7 @@ def split_csv(args, path):
         tables[data_name] = tables[data_name].set_axis(tables[data_name].iloc[0].to_list(), axis=1)
         tables[data_name] = tables[data_name].iloc[1:]
         tables[data_name] = tables[data_name].dropna(axis=1)
-        tables[data_name].to_csv(osp.join(args.root_dir,data_name+'.csv'), index = False)
+        tables[data_name].to_csv(osp.join(args.root_dir,data_name+'_'+str(id)+'.csv'), index = False)
 
 if __name__ == '__main__':
     import argparse
@@ -31,5 +32,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
     args.root_dir = './data/'
-    path = 'test221120/3.csv'
-    split_csv(args, path)
+    for id in [1,2,3,5,7,9,11,12]:
+        path = 'test221120/Sensordata_'
+        split_csv(args, path,id)
